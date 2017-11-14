a4c2f8605b69c91728e8bc3282eaf691ab27e136
sebgreaves1 <therealsebgreaves@gmail.com>
2017-11-14 00:43:31 +0000

    Webpack setup

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Test from './app/components/test'
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+
+ReactDOM.render(<Test/>, div);
